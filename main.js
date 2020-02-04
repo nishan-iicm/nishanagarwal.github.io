1ba9760cc36407589c31eb46f6bf148816c73245
nishan-iicm <46849594+nishan-iicm@users.noreply.github.com>
2020-02-05 01:13:54 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.progress-bar').waypoint(function () {
+        $('.progress-bar').css({
+            animation: "animate-positive 2s",
+            opacity: "1"
+        });
+    }, { offset: '55%' });
+}
+)
